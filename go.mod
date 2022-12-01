865de60e59d7cfa3b87ca9c1b7a8901eeea50717
zhazhalaila <1176694275@qq.com>
2022-12-01 17:17:57 +0800

    add proto

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.14
 
 require (
 	github.com/fortytw2/leaktest v1.3.0
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/stretchr/testify v1.8.0
 	go.uber.org/goleak v1.2.0
+	google.golang.org/protobuf v1.28.1
 )
---
+++ message_test/message_test.go
@@ -0,0 +1,38 @@
+package message_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	pb "github.com/stuck/message/messagepb"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
+)
+
+// Tests whether the any type can be marshal and unmarshal properly
+func TestAnyWork(t *testing.T) {
+	op := &pb.Operation{}
+	op.Op = pb.OpType_HandleRelationship
+	payloads := &pb.AnyTest{}
+	payloads.Message = "test"
+
+	// create any instance
+	any, err := anypb.New(payloads)
+	assert.Nil(t, err)
+	op.Payloads = any
+
+	// marshal
+	out, err := proto.Marshal(op)
+	assert.Nil(t, err)
+
+	// unmarshal
+	inOp := &pb.Operation{}
+	err = proto.Unmarshal(out, inOp)
+	assert.Nil(t, err)
+
+	// unmarshal any instance
+	inPayloads := &pb.AnyTest{}
+	err = inOp.Payloads.UnmarshalTo(inPayloads)
+	assert.Nil(t, err)
+	assert.Equal(t, "test", inPayloads.Message)
+}
